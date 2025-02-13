f7503bd61e88658d7353f85a4a63eaf42feaeee8
Ryu <112185361+HrLuna@users.noreply.github.com>
2025-02-13 14:27:45 +0800

    pusher

--- Visitor Management System/AddVisitor.cs
+++ Visitor Management System/AddVisitor.cs
@@ -124,89 +124,96 @@ namespace Visitor_Management_System
 
             timeVisit = DateTime.Now.ToString("hh:mm tt");
 
-            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(middleInitial) || string.IsNullOrEmpty(Email)
+            try
+            {
+                if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(middleInitial) || string.IsNullOrEmpty(Email)
                 || string.IsNullOrEmpty(Address) || string.IsNullOrEmpty(ContactPerson) || string.IsNullOrEmpty(Room) || string.IsNullOrEmpty(Purpose) || string.IsNullOrEmpty(CardNumber))
-            {
-                MessageBox.Show("Please fill the visitor credentials", "Add Visitor Failed", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-
-            else if (firstName.Any(char.IsDigit) && lastName.Any(char.IsDigit) && ContactPerson.Any(char.IsDigit) && middleInitial.Any(char.IsDigit))
-            {
-                MessageBox.Show("Check the credentials.", "Add Visitor Failed", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-
-            else if (!Email.Contains("@gmail.com"))
-            {
-                MessageBox.Show("Enter the gmail account of the visitor.", "Add Visitor Failed", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-
-            else if (ContactNum.Any(char.IsLetter))
-            {
-                MessageBox.Show("Contact Number must be numeric.", "Add Visitor Failed", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-            else
-            {
-                try
                 {
-                    MySqlConnection mysql = new MySqlConnection(mySqlCon);
+                    MessageBox.Show("Please fill the visitor credentials", "Add Visitor Failed", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
 
-                    mysql.Open();
-                    MySqlCommand cmd = new MySqlCommand("INSERT INTO addvisitor (FirstName, LastName, MiddleInitial, Suffix, Email, ContactNumber, DateofBirth, Address, VisitorImage, ContactPerson, IDPresented, Room, Department, Date, TimeIn, Purpose, CardNumber) " +
-                    "VALUES (@FirstName, @LastName, @MiddleInitial, @Suffix, @Email, @ContactNumber, @DateofBirth, @Address, @VisitorImage, @ContactPerson, @IDPresented, @Room, @Department, @Date, @TimeIn, @Purpose, @CardNumber)", mysql);
+                else if (firstName.Any(char.IsDigit) && lastName.Any(char.IsDigit) && ContactPerson.Any(char.IsDigit) && middleInitial.Any(char.IsDigit))
+                {
+                    MessageBox.Show("Check the credentials.", "Add Visitor Failed", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
 
-                    cmd.Parameters.AddWithValue("@FirstName", firstName);
-                    cmd.Parameters.AddWithValue("@LastName", lastName);
-                    cmd.Parameters.AddWithValue("@MiddleInitial", middleInitial);
-                    cmd.Parameters.AddWithValue("@Suffix", comboBox_suffix.Text.Trim());
-                    cmd.Parameters.AddWithValue("@Email", Email);
-                    cmd.Parameters.AddWithValue("@ContactNumber", ContactNum);
-                    cmd.Parameters.AddWithValue("@DateofBirth", dateofBirth);
-                    cmd.Parameters.AddWithValue("@Address", Address);
+                else if (!Email.Contains("@gmail.com"))
+                {
+                    MessageBox.Show("Enter the gmail account of the visitor.", "Add Visitor Failed", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
 
-                    if (pictureBox_userProfile.Image != null)
+                else if (ContactNum.Any(char.IsLetter))
+                {
+                    MessageBox.Show("Contact Number must be numeric.", "Add Visitor Failed", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    try
                     {
-                        using (MemoryStream ms = new MemoryStream())
-                        {
-                            Image resizedImage = new Bitmap(pictureBox_userProfile.Image, new Size(150, 150));
-                            resizedImage.Save(ms, ImageFormat.Jpeg);
+                        MySqlConnection mysql = new MySqlConnection(mySqlCon);
 
-                            if (ms.Length < 65536) 
+                        mysql.Open();
+                        MySqlCommand cmd = new MySqlCommand("INSERT INTO addvisitor (FirstName, LastName, MiddleInitial, Suffix, Email, ContactNumber, DateofBirth, Address, VisitorImage, ContactPerson, IDPresented, Room, Department, Date, TimeIn, Purpose, CardNumber) " +
+                        "VALUES (@FirstName, @LastName, @MiddleInitial, @Suffix, @Email, @ContactNumber, @DateofBirth, @Address, @VisitorImage, @ContactPerson, @IDPresented, @Room, @Department, @Date, @TimeIn, @Purpose, @CardNumber)", mysql);
+
+                        cmd.Parameters.AddWithValue("@FirstName", firstName);
+                        cmd.Parameters.AddWithValue("@LastName", lastName);
+                        cmd.Parameters.AddWithValue("@MiddleInitial", middleInitial);
+                        cmd.Parameters.AddWithValue("@Suffix", comboBox_suffix.Text.Trim());
+                        cmd.Parameters.AddWithValue("@Email", Email);
+                        cmd.Parameters.AddWithValue("@ContactNumber", ContactNum);
+                        cmd.Parameters.AddWithValue("@DateofBirth", dateofBirth);
+                        cmd.Parameters.AddWithValue("@Address", Address);
+
+                        if (pictureBox_userProfile.Image != null)
+                        {
+                            using (MemoryStream ms = new MemoryStream())
                             {
-                                cmd.Parameters.AddWithValue("@VisitorImage", ms.ToArray());
-                            }
-                            else
-                            {
-                                MessageBox.Show("Image is too large. Please use a smaller image.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                                return;
+                                Image resizedImage = new Bitmap(pictureBox_userProfile.Image, new Size(150, 150));
+                                resizedImage.Save(ms, ImageFormat.Jpeg);
+
+                                if (ms.Length < 65536)
+                                {
+                                    cmd.Parameters.AddWithValue("@VisitorImage", ms.ToArray());
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Image is too large. Please use a smaller image.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                    return;
+                                }
                             }
                         }
+                        else
+                        {
+                            cmd.Parameters.AddWithValue("@VisitorImage", DBNull.Value);
+                        }
+
+                        cmd.Parameters.AddWithValue("@ContactPerson", ContactPerson);
+                        cmd.Parameters.AddWithValue("@IDPresented", IdPresented);
+                        cmd.Parameters.AddWithValue("@Room", Room);
+                        cmd.Parameters.AddWithValue("@Department", department);
+                        cmd.Parameters.AddWithValue("@Date", dateVisit);
+                        cmd.Parameters.AddWithValue("@TimeIn", timeVisit);
+                        cmd.Parameters.AddWithValue("@Purpose", Purpose);
+                        cmd.Parameters.AddWithValue("@CardNumber", CardNumber);
+
+                        cmd.ExecuteNonQuery();
+
+                        mysql.Close();
                     }
-                    else
+                    catch (Exception ex)
                     {
-                        cmd.Parameters.AddWithValue("@VisitorImage", DBNull.Value);
+                        MessageBox.Show("Error: " + ex.Message, "Add Visitor Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     }
 
-                    cmd.Parameters.AddWithValue("@ContactPerson", ContactPerson);
-                    cmd.Parameters.AddWithValue("@IDPresented", IdPresented);
-                    cmd.Parameters.AddWithValue("@Room", Room);
-                    cmd.Parameters.AddWithValue("@Department", department);
-                    cmd.Parameters.AddWithValue("@Date", dateVisit);
-                    cmd.Parameters.AddWithValue("@TimeIn", timeVisit);
-                    cmd.Parameters.AddWithValue("@Purpose", Purpose);
-                    cmd.Parameters.AddWithValue("@CardNumber", CardNumber);
+                    FormLoader.LoadForm(modal_panel, new AddUserPrompt());
+                    modal_panel.Visible = true;
 
-                    cmd.ExecuteNonQuery();
-
-                    mysql.Close();
                 }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Error: " + ex.Message, "Add Visitor Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-
-                FormLoader.LoadForm(modal_panel, new AddUserPrompt());
-                modal_panel.Visible = true;
-
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Visitor Failed.", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
